26a7db975a361a56904bb1bbbde734a74fd6e78e
Lucky <lucky091588@gmail.com>
2020-11-05 15:34:17 +0800

    Remove alterMenu and add dependency on mosaicohooks ext

--- tokendefault.php
+++ tokendefault.php
@@ -208,16 +208,80 @@ function tokendefault_civicrm_alterAngular(\Civi\Angular\Manager $angular) {
   CRM_Core_Resources::singleton()->addScriptFile('com.joineryhq.tokendefault', 'js/tokendefault-utils.js');
 }
 
-/**
- * Implements hook_civicrm_alterMenu().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_alterMenu
- */
-function tokendefault_civicrm_alterMenu(&$items) {
-  // Override CRM_Mosaico_Page_EditorIframe with our own CRM_Tokendefault_Mosaico_Page_EditorIframe.
-  $items['civicrm/mosaico/iframe']['page_callback'] = 'CRM_Tokendefault_Mosaico_Page_EditorIframe';
+function tokendefault_civicrm_mosaicoConfigAlter(&$config) {
+  $config['tinymceConfig']['external_plugins']['tokendefault'] = CRM_Core_Resources::singleton()->getUrl('com.joineryhq.tokendefault', 'js/tinymce-plugins/tokendefault/plugin.js', 1);
+  $config['tinymceConfig']['plugins'][0] .= ' tokendefault';
+  $config['tinymceConfig']['toolbar1'] .= ' tokendefault';
+  $config['tinymceConfig']['tokendefault'] = true;
 }
 
+function tokendefault_civicrm_mosaicoScriptUrlsAlter(&$scriptUrls) {
+  $res = CRM_Core_Resources::singleton();
+
+  $coreResourceList = $res->coreResourceList('html-header');
+  $coreResourceList = array_filter($coreResourceList, 'is_string');
+  foreach ($coreResourceList as $item) {
+    if (
+      FALSE !== strpos($item, 'js')
+      && !strpos($item, 'crm.menubar.js')
+      && !strpos($item, 'crm.wysiwyg.js')
+      && !strpos($item, 'l10n-js')
+    ) {
+      if ($res->isFullyFormedUrl($item)) {
+        $itemUrl = $item;
+      }
+      else {
+        $item = CRM_Core_Resources::filterMinify('civicrm', $item);
+        $itemUrl = $res->getUrl('civicrm', $item, TRUE);
+      }
+      $scriptUrls[] = $itemUrl;
+    }
+  }
+
+  // Include our own JS.
+  $url = $res->addCacheCode(CRM_Utils_System::url('civicrm/tokendefault/mosaico-js', '', TRUE, NULL, NULL, NULL, NULL));
+  $scriptUrls[] = $url;
+}
+
+function tokendefault_civicrm_mosaicoStyleUrlsAlter(&$styleUrls) {
+  $res = CRM_Core_Resources::singleton();
+
+  // Load custom or core css
+  $config = CRM_Core_Config::singleton();
+  if (!Civi::settings()->get('disable_core_css')) {
+    $styleUrls[] = $res->getUrl('civicrm', 'css/civicrm.css', TRUE);
+  }
+  if (!empty($config->customCSSURL)) {
+    $customCSSURL = $res->addCacheCode($config->customCSSURL);
+    $styleUrls[] = $customCSSURL;
+  }
+  // crm-i.css added ahead of other styles so it can be overridden by FA.
+  array_unshift($styleUrls, $res->getUrl('civicrm', 'css/crm-i.css', TRUE));
+
+
+  $coreResourceList = $res->coreResourceList('html-header');
+  $coreResourceList = array_filter($coreResourceList, 'is_string');
+  foreach ($coreResourceList as $item) {
+    if (
+      FALSE !== strpos($item, 'css')
+      // Exclude jquery ui theme styles, which conflict with Mosaico styles.
+      && FALSE === strpos($item, '/jquery-ui/themes/')
+    ) {
+      if ($res->isFullyFormedUrl($item)) {
+        $itemUrl = $item;
+      }
+      else {
+        $item = CRM_Core_Resources::filterMinify('civicrm', $item);
+        $itemUrl = $res->getUrl('civicrm', $item, TRUE);
+      }
+      $styleUrls[] = $itemUrl;
+    }
+  }
+
+  // Include our own abridged styles from jquery-ui 'smoothness' theme, as
+  // required for our jquery-ui dialog, but which don't conflict with Mosaico.
+  $styleUrls[] = $res->getUrl('com.joineryhq.tokendefault', 'css/jquery-ui-smoothness-partial.css', TRUE);
+}
 
 /**
  * Implements hook_civicrm_navigationMenu().
